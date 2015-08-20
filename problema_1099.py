a2539a6af466920a79e04ce323cc66ff8ff53658
Welton Vaz de Souza <weltonvaz@gmail.com>
2015-08-20 17:43:37 -0300

    alterando arquivos em 20/08/2015

---
+++ problema_1099.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Leia um valor inteiro N que é a quantidade de casos de teste que vem a
+seguir. Cada caso de teste consiste de dois inteiros X e Y. Você deve apresentar
+ a soma de todos os ímpares existentes entre X e Y."""
+
+import os
+os.system('clear')
