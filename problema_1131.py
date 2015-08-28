51934f5b81fc4e98725d204613a2d75da4704240
Welton Vaz de Souza <weltonvaz@gmail.com>
2015-08-28 12:58:41 -0300

    alterando arquivos em 28/08/2015

--- problema_1131.py
+++ problema_1131.py
@@ -11,3 +11,53 @@ imprimindo:"""
 
 import os
 os.system('clear')
+
+import sys
+
+inter = 0
+gremio = 0
+empates = 0
+timev = ""
+
+def pergunta():
+    global inter
+    global gremio
+    global empates
+    global timev
+
+    placar = input()
+    i,g = placar.split()
+    vermelho = int(i)
+    azul = int(g)
+    if vermelho > azul:
+        inter += 1
+    elif vermelho == azul:
+        empates += 1
+    else:
+        gremio += 1
+    answer()
+
+def answer():
+    answer = input("Novo grenal (1-sim 2-nao)\n")
+    if answer == '1':
+        pergunta()
+    elif answer == '2':
+        resposta()
+        sys.exit()
+    else:
+        answer()
+
+def resposta():
+    total = inter+gremio+empates
+    print("%d grenais" %total)
+    print("Inter:%d" %inter)
+    print("Gremio:%d" %gremio)
+    print("Empates:%d" %empates)
+    if inter > gremio:
+        print("Inter venceu mais")
+    elif inter == gremio:
+        print("Nao houve vencedor")
+    else:
+        print("Gremio venceu mais")
+
+pergunta()
