573a6ae474d28f718e6a9103c7edb7d2a497d3a1
Welton Vaz de Souza <weltonvaz@gmail.com>
2015-09-16 15:54:22 -0300

    alterando arquivos em 16/09/2015

---
+++ problema_1181.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+
+"""Neste problema você deve ler um número, indicando uma linha da matriz na
+qual uma operação deve ser realizada, um caractere maiúsculo, indicando a
+operação que será realizada, e todos os elementos de uma matriz M[12][12].
+Em seguida, calcule e mostre a soma ou a média dos elementos que estão na área
+verde da matriz, conforme for o caso. A imagem abaixo ilustra o caso da entrada
+do valor 2 para a linha da matriz, demonstrando os elementos que deverão ser
+considerados na operação."""
+
+import os
+os.system('clear')
