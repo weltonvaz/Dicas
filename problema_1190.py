c73892b29fe2df119452b1d18eb6d25028bed315
Welton Vaz de Souza <weltonvaz@gmail.com>
2015-09-24 11:05:20 -0300

    alterando arquivos em 24/09/2015

--- problema_1190.py
+++ problema_1190.py
@@ -6,6 +6,9 @@ uma matriz M[12][12]. Em seguida, calcule e mostre a soma ou a média
 considerando somente aqueles elementos que estão acima da diagonal secundária da
 matriz, conforme ilustrado abaixo (área verde)."""
 
+import os
+os.system('clear')
+
 def soma():
     somam = 0
     matriz = []
